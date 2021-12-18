6cbc0fb2e3877d1b322de92dabff20404ec8a668
Chenxi Han <5656hcx@gmail.com>
2021-12-18 15:47:25 -0800

    Update state transition and event distribution

--- src/VideoPlayer.java
+++ src/VideoPlayer.java
@@ -10,48 +10,52 @@ class VideoPlayer extends MediaPlayer<ArrayList<File>> implements Runnable, Chan
     private final JLabel canvas;
     private ArrayList<File> videoFrames;
 
+    private Thread playbackThread;
+
+    /* SYNCHRONIZATION PARAMETERS */
     long lastSyncTime = -1;         // time of current frame rendered on screen
     long elapsedTimeActual = 0;     // actual elapsed time of the movie, depends on runtime environment
     int elapsedTimeStandard = 0;    // standard elapsed time of movie, calculated by pre-defined FPS
     int elapsedTimeReference = 0;   // target display time for every frame, regardless of I/O time or something
     int frameDurationOffset = 0;    // average offset between ideal and actual time to display a frame in target FPS
 
-    private Thread playbackThread;
-
     /* DEBUG ONLY PARAMETERS */
-    int fakeFrameIndex = 0;
+    int renderedFrameCount = 0;
 
     public VideoPlayer(Slider slider, JLabel canvas) {
-        currentState = State.Stopped;
         this.canvas = canvas;
         this.slider = slider;
         slider.addChangeListener(this);
+        setAndNotifyStateChanged(State.Stopped);
     }
 
     private void setAndNotifyStateChanged(State newState) {
-        if (currentState != newState) {
-            currentState = newState;
-            notifyStateChanged();
-        }
+        currentState = newState;
+        notifyStateChanged();
+    }
+
+    private void updateCanvas(long frameIndex) {
+        BufferedImage newImage = MediaReader.getInstance().readRgbFile(videoFrames.get((int) frameIndex));
+        canvas.setIcon(new ImageIcon(newImage));
     }
 
     @Override
-    void open(ArrayList<File> mediaSource) {
-        synchronized (this) {
+    public void open(ArrayList<File> mediaSource) {
+        synchronized (slider) {
             videoFrames = mediaSource;
             slider.reset(videoFrames);
+            reset();
         }
-        reset();
     }
 
     @Override
-    void close() {
+    public void close() {
 
     }
 
     @Override
     public void reset() {
-        synchronized (this) {
+        synchronized (slider) {
             setAndNotifyStateChanged(State.Paused);
             lastSyncTime = -1;
             peek(0);
@@ -66,19 +70,21 @@ class VideoPlayer extends MediaPlayer<ArrayList<File>> implements Runnable, Chan
     @Override
     protected void peek(long frameIndex) {
         if (canvas != null) {
-            BufferedImage newImage = MediaReader.getInstance().readRgbFile(videoFrames.get((int) frameIndex));
-            canvas.setIcon(new ImageIcon(newImage));
+            updateCanvas(frameIndex);
         }
-        if (slider != null && slider.getValue() != frameIndex) {
-            slider.setValue((int) frameIndex);
+        if (slider != null) {
+            synchronized (slider) {
+                if (slider.getValue() != frameIndex) {
+                    slider.setValue((int) frameIndex);
+                }
+            }
         }
     }
 
     @Override
     public void play() {
-        if (currentState != State.Playing) {
-            setAndNotifyStateChanged(State.Playing);
-            synchronized (this) {
+        synchronized (this) {
+            if (currentState != State.Playing) {
                 lastSyncTime = -1;
                 this.notifyAll();
             }
@@ -87,11 +93,7 @@ class VideoPlayer extends MediaPlayer<ArrayList<File>> implements Runnable, Chan
 
     @Override
     public void pause() {
-        if (currentState != State.Paused) {
-            synchronized (this) {
-                setAndNotifyStateChanged(State.Paused);
-            }
-        }
+        currentState = State.Paused;
     }
 
     @Override
@@ -105,42 +107,46 @@ class VideoPlayer extends MediaPlayer<ArrayList<File>> implements Runnable, Chan
         // won't reach Stopped state after execution
         // to DEBUG, call stop() to exit the thread
         while (currentState != State.Stopped) {
-            synchronized (VideoPlayer.this) {
-                int standard = getFrameDurationBiased(slider.getValue());
-                int cali = 0;
+            int standard = getFrameDurationBiased(slider.getValue());
+            synchronized (slider) {
                 if (lastSyncTime > 0) {
-                    BufferedImage newImage = MediaReader.getInstance().readRgbFile(videoFrames.get(slider.getValue()));
-                    canvas.setIcon(new ImageIcon(newImage));
+                    updateCanvas(slider.getValue());
                     if (slider.getValue() < slider.getMaximum()) {
+                        /* update synchronization parameters */
                         long now = System.currentTimeMillis();
                         long actual = now - lastSyncTime;
                         lastSyncTime = now;
-
                         elapsedTimeActual += actual;
                         elapsedTimeReference += standard;
                         elapsedTimeStandard += getFrameDurationStandard(slider.getValue());
-                        fakeFrameIndex++;
+                        renderedFrameCount++;
+                        frameDurationOffset = (int) (elapsedTimeActual - elapsedTimeReference) / renderedFrameCount;
                         slider.forward();
-                        frameDurationOffset = (int) (elapsedTimeActual - elapsedTimeReference) / fakeFrameIndex;
-                        cali = (int) (elapsedTimeActual - elapsedTimeStandard);
 
-                        if (standard - cali < 0) {
-                            cali = standard;
+                        /* calculate duration compensation for next frame */
+                        int compensation = (int) (elapsedTimeActual - elapsedTimeStandard);
+                        standard -= compensation;
+                        if (standard <= 0) {
+                            // delay is too much, discard next frame
                             System.out.printf("DELAYED! SKIP CURRENT FRAME: %d\n", slider.getValue());
-                        } else
-                            System.out.printf("Average FPS: %f, Accumulated Delay: %dms\n",
-                                    1000 / ((float) elapsedTimeActual / fakeFrameIndex), cali);
-                    }
-                    else setAndNotifyStateChanged(State.Paused);
-                }
-                else lastSyncTime = System.currentTimeMillis();
+                        } else {
+                            float fps = 1000 / ((float) elapsedTimeActual / renderedFrameCount);
+                            System.out.printf("Average FPS: %f, Accumulated Delay: %dms\n", fps, compensation);
+                        }
+                    } else setAndNotifyStateChanged(State.Paused);
+                } else lastSyncTime = System.currentTimeMillis();
+            }
+
+            synchronized (this) {
                 try {
                     if (currentState == State.Paused) {
+                        setAndNotifyStateChanged(State.Paused);
                         wait();
+                        setAndNotifyStateChanged(State.Playing);
                     }
                     else if (currentState == State.Playing) {
-                        if (standard - cali > 0) {
-                            wait(standard - cali);
+                        if (standard > 0) {
+                            wait(standard);
                         }
                     }
                 } catch (InterruptedException e) {
@@ -153,18 +159,19 @@ class VideoPlayer extends MediaPlayer<ArrayList<File>> implements Runnable, Chan
     }
 
     private int getFrameDurationBiased(int frameIndex) {
+        // 30 Frame Per Second
         return ((frameIndex % 3 == 1) ? 34 : 33) - frameDurationOffset;
     }
 
     private int getFrameDurationStandard(int frameIndex) {
+        // 30 Frame Per Second
         return (frameIndex % 3 == 1) ? 34 : 33;
     }
 
     @Override
     public void stateChanged(ChangeEvent e) {
-        if (canvas != null && currentState == State.Paused) {
-            BufferedImage newImage = MediaReader.getInstance().readRgbFile(videoFrames.get(slider.getValue()));
-            canvas.setIcon(new ImageIcon(newImage));
+        if (currentState == State.Paused && canvas != null) {
+            updateCanvas(slider.getValue());
         }
     }
 }
